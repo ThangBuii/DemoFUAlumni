4e7c8582dbff86a80e4fd1c0f81c9ae746dde7a8
Thang Bui <thangbdhe170642@fpt.edu.vn>
2024-11-01 02:27:33 +0700

    First commit

---
+++ ApiAngular/Models/Data/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiAngular.Models.Data
+{
+    public partial class User
+    {
+        public User()
+        {
+            Notifications = new HashSet<Notification>();
+            Posts = new HashSet<Post>();
+            Tags = new HashSet<Tag>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string StudentNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int Role { get; set; }
+
+        public virtual ICollection<Notification> Notifications { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
